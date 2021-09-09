d90c3482dd30c05602161f6ab8c36c3609e031ca
RobertDraper <rob.draper@live.com>
2021-09-09 15:02:36 +0100

    first successful flask test

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SOOCRET KOO'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,59 @@
-from application import app, db
+from flask_testing import TestCase
+from flask import url_for
 import unittest
+from application import app, db
+from application.models import Artists, Songs
 
-class FlaskTesting(unittest.TestCase):
-    #testing routes
-    def test_index_route(self):
-        tester = app.test_client(self)
-        response = tester.get("/index", content_type="html/text")
-        self.assertEqual(response.status_code, 200)
+class TestBase(TestCase):
 
+    def create_app(self):
+        # Defines the flask object's configuration for the unit tests
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI='sqlite:///test.db'
+            ,
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
+        )
+        return app
+    def setUp(self):
+        # Will be called before every test
+        # Create table schema
+        db.create_all()
+        # Create test data
+        #Songs(song_name="Las", album_name="Housses", trivia="trivia", artists_id=1)
+        test_artist = Artists(artist_name="Houses", individuals_in_group="individuals_in_group", year_founded=1951)
 
+        # save sample data to database
+        db.session.add(test_artist)
+        db.session.commit()
 
-    def test_base_route(self):
-        tester = app.test_client(self)
-        response = tester.get("/", content_type="html/text")
-        self.assertEqual(response.status_code, 200)
+    def tearDown(self):
+             db.session.remove()
+             db.drop_all()
 
-    def test_artists_route(self):
-        tester = app.test_client(self)
-        response = tester.get("/artists", content_type="html/text")
+class TestViews(TestBase):
+     def test_artist_get(self):
+         response = self.client.get(url_for("artists"))
+         self.assertEqual(response.status_code, 200)
+         self.assertIn(b'Houses', response.data)
+             
+
+# class FlaskTesting(unittest.TestCase):
+#     #testing routes
+#     def test_index_route(self):
+#         tester = app.test_client(self)
+#         response = tester.get("/index", content_type="html/text")
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_base_route(self):
+#         tester = app.test_client(self)
+#         response = tester.get("/", content_type="html/text")
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_artists_route(self):
+#         tester = app.test_client(self)
+#         response = tester.get("/artists", content_type="html/text")
+#         self.assertEqual(response.status_code, 200)
       
 
 # if __name__ == "__main__":
