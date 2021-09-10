d5c280600c111d3c966fd81d77ae61c41caccf56
RobertDraper <rob.draper@live.com>
2021-09-10 11:59:44 +0100

    testing

--- autotest.sh
+++ autotest.sh
@@ -1,7 +1,7 @@
 echo 'Hello from the best autotest.sh! Making this longer to do a push. Another test attempt going once, goin twice, going three times!
  Well, it needs to be tested again, and again!'
 
-sudo apt get install python3-pip3 python3-venv 
+apt get install python3-pip3 python3-venv 
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
