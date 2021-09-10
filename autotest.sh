bd4f7bd8a828b5f38c074de4c3877ac8e78c8eaa
RobertDraper <rob.draper@live.com>
2021-09-10 11:53:40 +0100

    testing pipeline

--- autotest.sh
+++ autotest.sh
@@ -1,4 +1,7 @@
 echo 'Hello from the best autotest.sh! Making this longer to do a push. Another test attempt going once, goin twice, going three times!
  Well, it needs to be tested again, and again!'
 
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 pytest
