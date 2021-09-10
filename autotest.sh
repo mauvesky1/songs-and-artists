fa79e0df7cc7eb3abea8163457f202590f722c09
RobertDraper <rob.draper@live.com>
2021-09-10 12:13:58 +0100

    cov tests

--- autotest.sh
+++ autotest.sh
@@ -5,4 +5,4 @@ apt get install python3-pip3 python3-venv -y
 python3 -m venv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
-pytest
+pytest --cov-report term-missing --cov=application tests/
