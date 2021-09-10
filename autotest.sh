0e844393ac6af0c33fcd5f5e772ba316426845ec
RobertDraper <rob.draper@live.com>
2021-09-10 11:50:27 +0100

    testing pipeline

--- autotest.sh
+++ autotest.sh
@@ -1,2 +1,4 @@
 echo 'Hello from the best autotest.sh! Making this longer to do a push. Another test attempt going once, goin twice, going three times!
  Well, it needs to be tested again, and again!'
+
+pytest
