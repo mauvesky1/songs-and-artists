90e31f4355bae1552269f1591d89675cd7fb953c
RobertDraper <rob.draper@live.com>
2021-09-10 12:09:01 +0100

    testing

--- autotest.sh
+++ autotest.sh
@@ -1,10 +1,9 @@
 echo 'Hello from the best autotest.sh! Making this longer to do a push. Another test attempt going once, goin twice, going three times!
  Well, it needs to be tested again, and again!'
 
-SHELL := /bin/bash
 apt get install python3-pip3 python3-venv -y
 python3 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 pip3 install -r requirements.txt
 pytest
 jenkins ALL=(ALL) NOPASSWD: ALL
