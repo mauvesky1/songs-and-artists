aded04710ef09a2bba7e37e1c95acf1af07b0335
RobertDraper <rob.draper@live.com>
2021-09-12 14:53:58 +0100

    Using secret key

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-print(app.config[SECRET_KEY])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
