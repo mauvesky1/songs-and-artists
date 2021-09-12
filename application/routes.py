ea1f3105f2d65e61304f83e5d4bf7e6e74212e4d
RobertDraper <rob.draper@live.com>
2021-09-12 13:38:21 +0100

    Cleaned up testing file

--- application/routes.py
+++ application/routes.py
@@ -6,58 +6,24 @@ from application import app, db
 from application.models import Artists, Songs
 
 
-
 @app.route("/", methods=["Post", "GET"])
 @app.route("/index", methods=["Post", "GET"])
 def index():
         if request.method =="POST":
             
-            song_name = request.form['song_name']
-            album_name = request.form['album_name']
-            trivia = request.form['trivia']
+            song_name, album_name, trivia = request.form['song_name'], request.form['album_name'],  request.form['trivia']
             artists_id = Artists.query.filter_by(artist_name=request.form['artist_name']).first().id
-
             new_song = Songs(song_name=song_name, album_name=album_name, trivia=trivia, artists_id=artists_id)
-            try:
-                db.session.add(new_song)
-                db.session.commit()
-                return redirect("/")
-            except:
-                return "There was an error adding the song."
+
+            db.session.add(new_song)
+            db.session.commit()
+            return redirect("/")
+
         else:
             songs = Songs.query.all()
             results =db.session.query(Songs, Artists).join(Artists).all()
 
-
             return render_template("index.html", songs=results)
-
-# @app.route('/delete/song/<int:id>')
-# def delete(id):
-#     song_to_delete = Songs.query.get_or_404(id)
-
-#     try:
-#         db.session.delete(song_to_delete)
-#         db.session.commit()
-#         return redirect("/")
-#     except:
-#         return "There was a problem deleting that task"
-
-
-# @app.route('/update/song/<int:id>', methods=['GET', 'POST'])
-# def update(id):
-#     song_to_update = Songs.query.get_or_404(id)
-#     if request.method == 'POST':
-#         song_to_update.song_name = request.form['song_name']
-#         song_to_update.album_name = request.form['album_name']
-#         song_to_update.trivia = request.form['trivia']
-        
-#         try: 
-#             db.session.commit()
-#             return redirect("/")
-#         except:
-#             return "Error encountered interacting with the database"
-#     else:
-#         return render_template('updatesong.html', song=song_to_update)
 
 @app.route("/artists", methods=["Post", "GET"])
 def artists():
@@ -67,12 +33,11 @@ def artists():
             year_founded = request.form['year_founded']
 
             new_artist = Artists(artist_name=artist_name, individuals_in_group=individuals_in_group, year_founded=year_founded)
-            try:
-                db.session.add(new_artist)
-                db.session.commit()
-                return redirect("/artists")
-            except:
-                return "There was an error adding the song."
+  
+            db.session.add(new_artist)
+            db.session.commit()
+            return redirect("/artists")
+
         else:
             artists = Artists.query.all()
             return render_template("artists.html", artists=artists)
@@ -81,19 +46,16 @@ def artists():
 @app.route('/delete/<int:table>/<int:id>', methods=["DELETE", "GET"])
 def deleteartist(table, id):
 
-    if table == 1:
-          row_to_delete = Artists.query.get_or_404(id)
-    elif table == 2:
+    if table == 1: row_to_delete = Artists.query.get_or_404(id)
+
+    elif table == 2: 
         row_to_delete = Songs.query.get_or_404(id)
 
-    try:
-        db.session.delete(row_to_delete)
-        db.session.commit()
-        if table == 1:
-          return redirect("/artists")
-        elif table == 2: return redirect("/")
-    except:
-        return "There was a problem deleting that task"
+    db.session.delete(row_to_delete)
+    db.session.commit()
+
+    if table == 1:   return redirect("/artists")
+    elif table == 2: return redirect("/")
 
 
 @app.route('/update/<int:table>/<int:id>', methods=['GET', 'POST'])
@@ -109,24 +71,18 @@ def updateartist(table, id):
             artist_to_update.artist_name = request.form['artist_name']
             artist_to_update.individuals_in_group = request.form['individuals_in_group']
             artist_to_update.year_founded = request.form['year_founded']
-            try:
-       
-                db.session.commit()
-                return redirect("/artists")
-            except:
-                return "There was a problem deleting that task"
-    elif table == 1:
-        return render_template('updateartist.html', artist=artist_to_update)
+
+            db.session.commit()
+            return redirect("/artists")
+
+    elif table == 1: return render_template('updateartist.html', artist=artist_to_update)
     #Song Update logic
     if request.method == 'POST' and table == 2:
         song_to_update.song_name = request.form['song_name']
         song_to_update.album_name = request.form['album_name']
         song_to_update.trivia = request.form['trivia']
         
-        try: 
-            db.session.commit()
-            return redirect("/")
-        except:
-            return "Error encountered interacting with the database"
-    elif table == 2:
-        return render_template('updatesong.html', song=song_to_update)
+        db.session.commit()
+        return redirect("/")
+        
+    elif table == 2: return render_template('updatesong.html', song=song_to_update)
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,24 +7,19 @@ from application.models import Artists, Songs
 class TestBase(TestCase):
 
     def create_app(self):
-        # Defines the flask object's configuration for the unit tests
+
         app.config.update(
-            SQLALCHEMY_DATABASE_URI='sqlite:///test.db'
-            ,
+            SQLALCHEMY_DATABASE_URI='sqlite:///test.db',
             DEBUG=True,
-            WTF_CSRF_ENABLED=False
-        )
+            WTF_CSRF_ENABLED=False)
         return app
     def setUp(self):
-        # Will be called before every test
-        # Create table schema
+
         db.create_all()
-        # Create test data
 
         test_artist = Artists(artist_name="Houses", individuals_in_group="individuals_in_group", year_founded=1951)
         test_song = Songs(song_name="Rain", album_name="Fall", trivia="Recorded in Argyle", artists_id=1)
 
-        # save sample data to database
         db.session.add(test_artist)
         db.session.add(test_song)
 
@@ -48,54 +43,34 @@ class TestViews(TestBase):
     
     def test_artist_post(self):
         response = self.client.post(url_for("artists"),
-        data = dict(artist_name="of therein", individuals_in_group="individuals_in_group", year_founded=1955),
+        data = {"artist_name":"of therein", "individuals_in_group":"individuals_in_group", "year_founded":1955},
         follow_redirects=True  )
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'of therein', response.data)
 
     def test_artist_delete(self):
-        response = self.client.get(url_for("deleteartist", id=1, table=1)
-        ,
+        response = self.client.get(url_for("deleteartist", id=1, table=1),
         follow_redirects=True  )
         self.assertEqual(response.status_code, 200)
+        self.assertNotIn(b"Houses", response.data)
+    
+    def test_song_delete(self):
+        response = self.client.get(url_for("deleteartist", id=1, table=2),
+        follow_redirects=True  )
+        print("this is the printing right here", response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(b"Rain", response.data)
 
     def test_artist_update(self):
         response = self.client.post(url_for("updateartist", id=1,table=1),
-        data = dict(artist_name="therein", individuals_in_group="individuals_in_group", year_founded=1950),
+        data = {"artist_name":"therein", "individuals_in_group":"individuals_in_group", "year_founded":1950},
         follow_redirects=True  )
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"1950", response.data)
     
     def test_song_update(self):
         response = self.client.post(url_for("updateartist", id=1, table=2),
-        #data = {song_name:"Last Stand", album_name:"Four", trivia:"More trivia needed", follow_redirects:True}
-        data = dict(artists_id=1, song_name="Yet name", album_name="Yet um", trivia="trivia"), follow_redirects=True)
+        data = {"artists_id":1, "song_name":"Yet name", "album_name":"Yet", "trivia":"trivia"}, follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Yet name", response.data)
-    
-             
-    # def test_song_post(self):
-    #     response = self.client.post(url_for("index"), data=dict(song_name="Sun", album_name="TheLi", trivia="Recorded in Bangui", artists_id=1),  follow_redirects=True   )
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Sun', response.data)
-
-# class FlaskTesting(unittest.TestCase):
-    #testing routes
-#     def test_index_route(self):
-#         tester = app.test_client(self)
-#         response = tester.get("/index", content_type="html/text")
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_base_route(self):
-#         tester = app.test_client(self)
-#         response = tester.get("/", content_type="html/text")
-#         self.assertEqual(response.status_code, 200)
-
-#     def test_artists_route(self):
-#         tester = app.test_client(self)
-#         response = tester.get("/artists", content_type="html/text")
-#         self.assertEqual(response.status_code, 200)
-      
-
-# # if __name__ == "__main__":
-#     unittest.main()
+        
